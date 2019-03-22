39eb3c04e6bb05a6e685cb5d64a8ddd60c2baaf6
Eric Chiu <37917779+ericcchiu@users.noreply.github.com>
2019-03-21 21:23:08 -0500

    Initialized navigation bar content

---
+++ client/src/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const NavigationBar = () => {
+
+  return (
+    <div className='navigation-bar-container'>
+      <h1>Navigation Bar Content</h1>
+    </div>
+  );
+}
+
+export default NavigationBar; 
