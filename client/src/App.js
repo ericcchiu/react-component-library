dda621fa5b6073f8c8b4d57b736533dcca8bc70f
ericcchiu <eric.chn.chiu@gmail.com>
2019-03-24 16:16:14 -0500

    (UPDATE) Render new components to page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import NavigationBar from './NavigationBar';
-
+import NavigationBar from './components/NavigationBar';
+// import CarouselView from './components/refactor';
+import CarouselView from './components/CarouselView';
 const App = () => {
   return (
     <div className="App">
       <NavigationBar />
       <h1>Welcome to React Component library</h1>
+      <CarouselView />
     </div>
   )
 }
