bc766c52b7cc1c105e80c3126ce8cd939cea04c0
ericcchiu <eric.chn.chiu@gmail.com>
2019-03-24 12:41:18 -0500

    (REFACTOR) Refactor App component to use react hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import NavigationBar from './NavigationBar';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome to React Boiler Plate</h1>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <NavigationBar />
+      <h1>Welcome to React Component library</h1>
+    </div>
+  )
 }
 export default App; 
