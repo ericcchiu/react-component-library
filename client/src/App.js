25b0a8a311a50531f77ab623ec8a1bc130862d22
ericcchiu <eric.chn.chiu@gmail.com>
2019-03-25 17:59:45 -0500

    (UPDATE) Add forms to application

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Spinner } from 'react-bootstrap';
 import NavigationBar from './components/NavigationBar';
 import CarouselView from './components/CarouselComponent/CarouselViewRefactor';
+import SignInForm from './components/Forms/SignInForm';
+import FormView from './components/Forms/FormView';
 
 const App = () => {
   const [isLoading, setLoading] = useState(false);
@@ -12,6 +14,8 @@ const App = () => {
       <h1>Welcome to React Component library</h1>
       {isLoading && <Spinner animation="border" variant="success" />}
       {!(isLoading) && <CarouselView />}
+      <SignInForm />
+      <FormView />
     </div>
   )
 }
